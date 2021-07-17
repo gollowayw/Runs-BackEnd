fd50f2db76b8c930abc7a9bb81291344dc9def52
gollowayw <gollowayw@gmail.com>
2021-07-16 17:09:05 -0700

    environment variable set up

--- handler.py
+++ handler.py
@@ -1,19 +1,17 @@
 import pymysql
 import json
+import os 
+from dotenv import load_dotenv
 
-#Configuration Values
-endpoint = 'runs.clezslj1op7i.us-west-2.rds.amazonaws.com'
-username = 'admin'
-password = 'test1234'
-database_name = 'Application'
-
+load_dotenv()
 #Connection
-connection = pymysql.connect(host=endpoint,user=username,
-           password=password,database=database_name)
+connection = pymysql.connect(host=os.getenv('endpoint'),user=os.getenv('dbusername'),
+           password=os.getenv('password'),database=os.getenv('database_name'))
 
 # define the handler function that the Lambda service will use as an entry point
-def lambda_handler(event, context):
-      
+# add context back 
+def lambda_handler(event,context):
+    
     strUserName = event['strUserName']
     strAlias = event['strAlias']
     strImageUrl = event['strImageUrl']
@@ -26,7 +24,17 @@ def lambda_handler(event, context):
 
     return {
         'statusCode': 200,
-        'body': json.dumps('User Added: ' + event['strUserName'])
+        'body': json.dumps(event)
         }
-    
+
+
+# DUMMY_DATA =  {
+# "strUserName" : "localTest1",
+# "strAlias" : "localTest1",
+# "strImageUrl" : "localTest1",
+# "strBio" : "localTest1"
+
+#  }
+ 
+# lambda_handler(DUMMY_DATA)
     
